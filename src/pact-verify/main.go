898fffb3bc9a7a01ae50677fe06f87ce699f1eeb
Zia Grosvenor <ziagrosvenor@live.co.uk>
2016-05-08 20:48:47 +0100

    Add better error handling

--- src/pact-verify/main.go
+++ src/pact-verify/main.go
@@ -6,60 +6,11 @@ import (
 	"fmt"
 	"github.com/codegangsta/cli"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 )
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-var pactHelperTemplate = `
-%[1]v
-
-Pact.provider_states_for "%[2]v" do
-  %[3]v
-end
-`
-
-var template = `
-  provider_state "%[1]v" do
-    set_up do
-      set_up_state "%[2]v", "%[1]v"
-    end
-  end
-`
-
-var pactHelperBaseStr = `
-require 'faraday'
-require 'cgi'
-
-PROVIDER_STATE_SERVER_SET_UP_URL = ENV["SETUP_SERVER_URL"] 
-
-# Responsible for making the call to the provider state server to set up the state
-module ProviderStateServerClient
-
-  def set_up_state consumer_name, provider_state
-    puts "Setting up provider state '#{provider_state}' using provider state server at #{PROVIDER_STATE_SERVER_SET_UP_URL}"
-    Faraday.post(PROVIDER_STATE_SERVER_SET_UP_URL, {"consumer" => consumer_name, "provider_state" => provider_state })
-  end
-
-end
-
-Pact.configure do | config |
-  config.include ProviderStateServerClient
-end
-`
-
-func getRootDirPath() string {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	check(err)
-
-	return dir
-}
 
 func buildPactHelperFromPactJson(pactFilePath string) string {
 	var dir = getRootDirPath()
@@ -103,6 +54,13 @@ func writePactHelperFile(ROOT_DIR string, pactHelperStr string) {
 	check(err)
 }
 
+func printHelp() {
+	cmd := exec.Command("pact-verify", "help")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+}
+
 func main() {
 	var pactFilePath string
 	var providerUrl string
@@ -114,19 +72,19 @@ func main() {
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:        "pact",
-			Usage:       "Read from disc and process a Pact JSON file from `PATH`",
+			Usage:       "Read a Pact file from `PATH` and process it",
 			Destination: &pactFilePath,
 		},
 
 		cli.StringFlag{
 			Name:        "provider, prov",
-			Usage:       "The URL of the provider service that the pact will be verified against",
+			Usage:       "The `URL` of the provider service to verify the pact with",
 			Destination: &providerUrl,
 		},
 
 		cli.StringFlag{
 			Name:        "setup, s",
-			Usage:       "The URL of the provider state server - This is used to process provider states",
+			Usage:       "The `URL` of the provider state server - This is used to process provider states",
 			Destination: &stateServerUrl,
 		},
 	}
@@ -136,23 +94,15 @@ func main() {
 	app.Action = func(c *cli.Context) error {
 
 		if pactFilePath == "" {
-			fmt.Printf("\nEXITED \nA Pact file path required i.e /tmp/pacts/pact.json \n\n")
-
-			cmd := exec.Command("pact-verify", "help")
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			cmd.Run()
-			return nil
+			return cli.NewExitError("\nEXITED \nA Pact file path is required i.e /tmp/pacts/pact.json \n", 86)
 		}
 
 		if providerUrl == "" {
-			fmt.Printf("\nEXITED \nProvider url required\n\n")
+			return cli.NewExitError("\nEXITED \nA provider service URL is required\n", 86)
+		}
 
-			cmd := exec.Command("pact-verify", "help")
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			cmd.Run()
-			return nil
+		if stateServerUrl == "" {
+			return cli.NewExitError("\nEXITED \nA provider states setup service URL is required\n", 86)
 		}
 
 		var ROOT_DIR = os.Getenv("GOPATH")
@@ -178,3 +128,63 @@ func main() {
 
 	app.Run(os.Args)
 }
+
+var errorExitMessage = `
+EXITING due to an error.
+
+Please ensure all CLI options are correctly configured.
+Try pact-verify help for more information.
+
+ERROR:
+`
+
+func check(e error) {
+	if e != nil {
+		fmt.Println(errorExitMessage)
+		log.Fatal(e)
+	}
+}
+
+func getRootDirPath() string {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	check(err)
+
+	return dir
+}
+
+var pactHelperTemplate = `
+%[1]v
+
+Pact.provider_states_for "%[2]v" do
+  %[3]v
+end
+`
+
+var template = `
+  provider_state "%[1]v" do
+    set_up do
+      set_up_state "%[2]v", "%[1]v"
+    end
+  end
+`
+
+var pactHelperBaseStr = `
+require 'faraday'
+require 'cgi'
+
+PROVIDER_STATE_SERVER_SET_UP_URL = ENV["SETUP_SERVER_URL"] 
+
+# Responsible for making the call to the provider state server to set up the state
+module ProviderStateServerClient
+
+  def set_up_state consumer_name, provider_state
+    puts "Setting up provider state '#{provider_state}' using provider state server at #{PROVIDER_STATE_SERVER_SET_UP_URL}"
+    Faraday.post(PROVIDER_STATE_SERVER_SET_UP_URL, {"consumer" => consumer_name, "provider_state" => provider_state })
+  end
+
+end
+
+Pact.configure do | config |
+  config.include ProviderStateServerClient
+end
+`
